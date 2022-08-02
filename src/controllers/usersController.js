f30ba938b1ee353f6dd5b1db0e2d57c400b6b677
Mark <xenicmark@gmail.com>
2022-08-03 01:05:31 +0300

    reorganised code

---
+++ src/controllers/usersController.js
@@ -0,0 +1,50 @@
+import { json } from 'express';
+import User from '../models/Users';
+import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
+
+class usersController {
+  static async registerUser(req, res) {
+    const { username, email, password } = req.body;
+    const emailExists = await User.findOne({ email });
+    if (emailExists) return res.json({ message: 'Email already exists' });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+    try {
+      const user = await User.create({
+        userId: uuidv4(),
+        username,
+        email,
+        password: hashedPass,
+      });
+      //   const newUser = user.save();
+      return res.status(201).json({
+        userId: user.userId,
+        username: user.username,
+        email: user.email,
+        password: '********',
+      });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+
+  static async loginUser(req, res) {
+    const { username, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'User not found' });
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass)
+      return res.status(400).json({ message: 'Invalid Password' });
+    const token = jwt.sign({ userId: user.UserId }, process.env.TOKEN_SECRET);
+    return res.header('aut-token', token).json({
+      userId: user.userId,
+      username: user.username,
+      email: user.email,
+      password: '********',
+    });
+  }
+}
+
+export default usersController;
