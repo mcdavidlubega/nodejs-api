dd484f435690fd20cf9243210ce8c87e541e62ad
Mark <xenicmark@gmail.com>
2022-08-03 22:54:51 +0300

    implemented questions feature

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    userId: String,
     username: { type: String, min: 5, max: 50 },
     email: { type: String, min: 6, max: 50 },
     password: { type: String, min: 6, max: 1024 },
-    date: { type: Date, default: Date.now },
+    dateCreated: { type: Date, default: Date.now, immutable: true },
 });
 
 export default mongoose.model('User', userSchema);
--- src/routes/api-v1/index.js
+++ src/routes/api-v1/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRoute from './users';
+import questionsRoute from './questions';
 
 const routes = Router();
 
 routes.use('/users', usersRoute);
+routes.use('/questions', questionsRoute);
 
 export default routes;
