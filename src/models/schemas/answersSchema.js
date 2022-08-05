b1ea5fa260a053cc92747fbe9e74c2aa2a664bb7
Mark <xenicmark@gmail.com>
2022-08-05 04:22:02 +0300

    fixed bug in answers feature that duplicated votes in voting system

--- src/models/schemas/answersSchema.js
+++ src/models/schemas/answersSchema.js
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
 
-// const votesSchema = new mongoose.Schema({
-//     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-//     downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-// });
 const answersSchema = new mongoose.Schema({
     answer: { type: String, min: 3000 },
     questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question' },
