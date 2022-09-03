3b44a22e1134058cdd6dc65a46fc2764dfca5e9a
Mark <xenicmark@gmail.com>
2022-09-03 14:27:25 +0300

    added home route

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ connect();
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.get('/', (req, res) => {
+    return res.status(200).send('Home');
+});
 app.use((req, res) => res.status(404).json({ message: 'Resource not found' }));
 
 app.use(errors());
